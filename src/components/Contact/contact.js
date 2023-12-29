5b75a6e55e8e16bfb5676a0339e3e90bb34ac1e0
Ani2nem <ani2nem@gmail.com>
2023-12-29 14:58:17 -0600

    resloved issues with emailJs and fixed other warning bugs

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,14 +1,20 @@
 import "./contact.css"
-import React, { useRef } from 'react';
-import emailjs, { send } from '@emailjs/browser';
+import React, { useRef, useEffect } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () =>{
 
     const form = useRef();
+
+    useEffect(() => {
+        // Initialize EmailJS with your user ID
+        emailjs.init('user_ifTHd4HcJTlpmBvKs');
+    }, []);
+
     const sendEmail = (e) => {
         e.preventDefault();
     
-        emailjs.sendForm('service_y0n53x3', 'template_x9zbji6', form.current, 'qO4OpxNfUG01EON73tw6X')
+        emailjs.sendForm('service_y0n53x3', 'template_x9zbji6', form.current, 'ifTHd4HcJTlpmBvKs')
           .then((result) => {
               console.log(result.text);
               e.target.reset();
@@ -23,8 +29,8 @@ const Contact = () =>{
             <h1 className="contact-title">Contact</h1>
             <span className="contact-subtitle">Please fill out form below to discuss any work opportunities</span>
             <form className="contact-form" ref={form} onSubmit={sendEmail}>
-                <input type="text" className="name" placeholder="Your Name" name='your_name'/>
-                <input type="text" className="email" placeholder="Your Email" name='your_email'/>
+                <input type="text" className="name" placeholder="Your Name" name='from_name'/>
+                <input type="text" className="email" placeholder="Your Email" name='from_email'/>
                 <textarea className="msg" name="message" rows="5" placeholder='Your Message'></textarea>
                 <button type="submit" value='Send' className="submitBtn">Submit</button>
                 <p className="email-text">anirudh.nemmani@yahoo.com</p>
