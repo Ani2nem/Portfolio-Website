9c198a02321cd637fd01a3b271092c9bbaaf4c3a
Ani2nem <ani2nem@gmail.com>
2024-05-13 10:42:24 -0500

    fixed the icons bug in footer and hero

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -11,16 +11,18 @@ const Hero = () => {
         <text className="hero-text">
           A Software Engineer with a passion for unraveling complex problems. Proficient in Java for robust applications,
           Python for insightful data visualization, and adept in the MERN stack for full-stack web development.
-          <div className="icons">
-        <a href="https://www.linkedin.com/in/anirudhnemmani/" target="_blank" rel="noreferrer">
-            <img src="/assets/Icons/linkedinIcon.png" alt="linkedin" className="linkedinIcon" />
-            </a>
-        <a href="https://github.com/Ani2nem" target='_blank' rel="noreferrer">
-            <img src="/assets/Icons/githubIcon.png" alt="github" className="githubIcon" rel="noreferrer"/>
-            </a>
-        <a href="/assets/Tech_Resume.pdf" target="_blank" rel="noreferrer">
-            <img src="/assets/Icons/resumeIcon.png" alt="resume" className="resumeIcon" />
-        </a>
+        <div className="icon-holder">
+            <div className="icons">
+          <a href="https://www.linkedin.com/in/anirudhnemmani/" target="_blank" rel="noreferrer">
+              <img src="/assets/Icons/linkedinIcon.png" alt="linkedin" className="linkedinIcon" />
+              </a>
+          <a href="https://github.com/Ani2nem" target='_blank' rel="noreferrer">
+              <img src="/assets/Icons/githubIcon.png" alt="github" className="githubIcon" rel="noreferrer"/>
+              </a>
+          <a href="/assets/Tech_Resume.pdf" target="_blank" rel="noreferrer">
+              <img src="/assets/Icons/resumeIcon.png" alt="resume" className="resumeIcon" />
+          </a>
+          </div>
         </div>
           
         </text>
