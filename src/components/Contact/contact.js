346409ff421e932da3fff64a103ad9ce730f6269
Ani2nem <ani2nem@gmail.com>
2024-05-13 11:01:23 -0500

    fixed react scroll with the new mobile responsive interface

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -25,17 +25,18 @@ const Contact = () =>{
       };
 
     return (
-        <section className="contactSection" id="Contact">
-            <h1 className="contact-title">Contact</h1>
+        <div className="contactSection">
+            <h1 className="contact-title" id="Contact">Contact</h1>
             <span className="contact-subtitle">Please fill out form below to discuss any work opportunities</span>
             <form className="contact-form" ref={form} onSubmit={sendEmail}>
                 <input type="text" className="name" placeholder="Your Name" name='from_name'/>
                 <input type="text" className="email" placeholder="Your Email" name='from_email'/>
                 <textarea className="msg" name="message" rows="5" placeholder='Your Message'></textarea>
                 <button type="submit" value='Send' className="submitBtn">Submit</button>
-                <p className="email-text">anirudh.nemmani@yahoo.com</p>
+                <p className="email-text" >anirudh.nemmani@yahoo.com</p>
             </form>
-        </section>
+        </div>
+        
     )
 }
 
--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -3,13 +3,14 @@ import './navbar.css'
 import {Link} from 'react-scroll'
 
 const Navbar = () => {
+
   return (
     <nav className="navbar">
         <div className="desktopMenu">
-            <Link to="Home" spy={true} smooth={true} offset={100} duration={500} className="desktopMenuListItem">Home</Link>
-            <Link  to="About" spy={true} smooth={true} offset={0} duration={500} className="desktopMenuListItem">About</Link>
+            <Link to="Home" spy={true} smooth={true} offset={0} duration={500} className="desktopMenuListItem">Home</Link>
+            <Link to="About" spy={true} smooth={true} offset={0} duration={500} className="desktopMenuListItem">About</Link>
             <Link to="Skills" spy={true} smooth={true} offset={0} duration={500} className="desktopMenuListItem">Skills</Link>
-            <Link to="Contact" spy={true} smooth={true} offset={300} duration={500} className="desktopMenuListItem">Contact</Link>
+            <Link to="Contact" spy={true} smooth={true} offset={0} duration={500} className="desktopMenuListItem">Contact</Link>
         </div>
     </nav>
   )
