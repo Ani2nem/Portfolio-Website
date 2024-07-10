09df2c4ff20e2f7d26fbe70a9e0fa2f3ebeefca6
Anirudh Nemmani <96967353+Ani2nem@users.noreply.github.com>
2024-07-09 20:46:57 -0500

    set up for project carasoul section

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import AboutMe from "./components/AboutMe/aboutMe"
 import Skills from "./components/Skills/skills"
 import Contact from "./components/Contact/contact"
 import Footer from "./components/Footer/footer"
+import Projects from "./components/Projects/Projects"
 
 function App() {
   return (
       <div className="App">
         <Navbar />
         <Hero />
+        <Projects />
         <AboutMe />
         <Skills />
         <Contact />
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import './Projects.css';
+import Slider from "react-slick";
+
+const Projects = () => {
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+      };
+
+return (
+    <section>
+        <Slider
+          {...settings}
+          className="slider"
+        >
+            <div key={_id}>
+                <img
+                  src={image}
+                  alt={name}
+                  className="w-full rounded-lg object-cover h-[30rem]"
+                />
+
+                <div className="mt-4 flex justify-between px-[3rem]">
+                  <div className="one">
+                    <h2>{name}</h2>
+                    <p>  ₹{price}</p> <br /> <br />
+                    <p className="w-[25rem]">
+                      {description.substring(0, 170)} ...
+                    </p>
+                  </div>
+
+                  <div className="flex justify-between lg:w-[30rem] md:w-[50rem]">
+                    <div className="one">
+                      <h1 className="flex items-center mb-6">
+                        <FaStore className="mr-2 text-white" /> Brand: {brand}
+                      </h1>
+                      <h1 className="flex items-center mb-6">
+                        <FaClock className="mr-2 text-white" /> Added:{" "}
+                        {moment(createdAt).fromNow()}
+                      </h1>
+                      <h1 className="flex items-center mb-6">
+                        <FaStar className="mr-2 text-white" /> Reviews:
+                        {numReviews}
+                      </h1>
+                    </div>
+
+                    <div className="two">
+                      <h1 className="flex items-center mb-6">
+                        <FaStar className="mr-2 text-white" /> Ratings:{" "}
+                        {Math.round(rating)}
+                      </h1>
+                      <h1 className="flex items-center mb-6">
+                        <FaShoppingCart className="mr-2 text-white" /> Quantity:{" "}
+                        {quantity}
+                      </h1>
+                      <h1 className="flex items-center mb-6">
+                        <FaBox className="mr-2 text-white" /> In Stock:{" "}
+                        {countInStock}
+                      </h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+
+        </Slider>
+        
+    </section>
+
+)};
+
+export default Projects;
