acac62db36a3b19ecca1329c7ed76277ea5ab152
Anirudh Nemmani <96967353+Ani2nem@users.noreply.github.com>
2024-11-18 22:10:45 -0600

    fixed spelling error

--- src/components/AboutMe/aboutMe.js
+++ src/components/AboutMe/aboutMe.js
@@ -7,7 +7,7 @@ const AboutMe = () =>{
             <h1 className="aboutMeTitle" id="About">About Me</h1>  
             <div className='textContainer'>
             <p className="aboutMe">I am a Computer Science student 
-            graduating from The University of Texas at Dallas in Dec 2026 . Proficient in Java, 
+            graduating from The University of Texas at Dallas in Dec 2026. Proficient in Java, 
             JavaScript, Python, C++, and C, I excel in technologies such as AWS, MongoDB, React.js, 
             and Node.js. With a solid foundation in Agile methodologies, Git, and GitHub, I bring diverse 
             experience and a commitment to impactful solutions in computer science.</p>
