44c8799a83caf8893f34d5ae58d3615e1afdbf6a
Anirudh Nemmani <96967353+Ani2nem@users.noreply.github.com>
2025-03-24 11:27:24 -0500

    added spring boot and removed numpy icon

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -11,6 +11,7 @@ const Skills = () => {
                 <img src="/assets/Icons/pythonIcon.svg" alt="" className="python-icon"/>
                 <img src="/assets/Icons/mysqlIcon.svg" alt="" className="mysql-icon"/>
                 <img src="/assets/Icons/nodejsIcon.svg" alt="" className="node-icon"/>
+                <img src="/assets/Icons/springIcon.svg" alt="" className="spring-icon"/>
                 <img src="/assets/Icons/jsIcon.svg" alt="" className="js-icon"/>
                 <img src="/assets/Icons/cssIcon.svg" alt="" className="css-icon"/>
                 <img src="/assets/Icons/htmlIcon.svg" alt="" className="html-icon"/>
@@ -18,7 +19,6 @@ const Skills = () => {
                 <img src="/assets/Icons/andriodstudioIcon.svg" alt="" className="studio-icon"/>
                 <img src="/assets/Icons/mongodbIcon.svg" alt="" className="mongo-icon"/>
                 <img src="/assets/Icons/pandasIcon.ico" alt="" className="pandas-icon"/>
-                <img src="/assets/Icons/numpyIcon.svg" alt="" className="numpy-icon"/>
                 <img src="/assets/Icons/plotlyIcon.png" alt="" className="plotly-icon"/>
                 <img src="/assets/Icons/awsIcon.svg" alt="" className="aws-icon"/>
             </div>
